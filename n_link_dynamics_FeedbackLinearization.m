c96b13c6aaaf67884fffe552a01b80555d33d8fa
Asutay Ozmen <ozmen@ucsb.edu>
2021-08-30 13:23:17 -0700

    dynamics for ODE with FeedbackLin

---
+++ n_link_dynamics_FeedbackLinearization.m
@@ -0,0 +1,21 @@
+function [dQ] = n_link_dynamics_FeedbackLinearization(Q,M_fnc,C_fnc,Tg_fnc,B,g,q_vars,full_vrs)
+n = length(Q)/2;
+
+dq_act(1:n,1) = Q(n+1:end);
+q_act(1:n,1) = Q(1:n);
+
+% for k=1:n
+%     eval(sprintf('q%d = q_act(k);', k));
+%     eval(sprintf('dq%d = dq_act(k);', k));
+% end
+% M = double(subs(M)); C = double(subs(C)); Tg = double(subs(Tg)); 
+eval(sprintf(['Tg = Tg_fnc(' q_vars ');']));
+eval(sprintf(['M = M_fnc(' q_vars ');']));
+eval(sprintf(['C = C_fnc(' full_vrs ');']));
+
+u = double(C*dq_act + Tg*g);
+
+d2q = double(-M\C*dq_act-M\Tg*g+M\B*u);
+dQ = [dq_act; d2q];
+end
+
