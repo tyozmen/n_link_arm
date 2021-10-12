0234da29c658a31dda1aa02fc130de3cad64bb79
Asutay <ozmen@ucsb.edu>
2021-10-11 17:21:07 -0700

    minor changes to plotting

--- bball_1dof_apex/bball_1_dof_env_apex_control_test.m
+++ bball_1dof_apex/bball_1_dof_env_apex_control_test.m
@@ -5,16 +5,22 @@ env = bball_1_dof_Env_apex_control();
 %plot(env);
 
 reset(env);
-n_episodes = 1000;
+n_episodes = 2000;
 begin = tic;
 y_des = [env.y_des];
-h_apx = [env.h_apx+env.y_imp];
+h_apx = [env.h_apx];
+offset = env.y_imp+env.d+env.r;
+h_d_apx = [(env.h_d_apx+offset)];
 R = 0
 for i=1:n_episodes
     [s,r,d,~] = step(env,0);
     y_des = [y_des env.y_des];
-    h_apx = [h_apx env.h_apx+env.y_imp];
+    h_apx = [h_apx env.h_apx];
+    h_d_apx = [h_d_apx env.h_d_apx+offset];
     R = R + r;
+    if i> 450
+        env.h_d_apx =  5;
+    end
 %     if mod(i,5) == 0
 %     end
 end
@@ -22,18 +28,18 @@ end
 fprintf("FPS: %f \n",  n_episodes/toc(begin));
 
 [states, actions, t] = get_arrays(env);
-
+figure()
 plot(t,states(1,:),t,y_des);
 title('link position');
 xlabel('t (s)'); ylabel('y (m)');
 legend('y-actual)', 'y-desired)');
 figure();
-plot(h_apx); hold on; yline(env.h_d_apx+env.y_imp,'.-r')
-title('calculated apex height values based on states vs desired apex height');
+plot(t,h_apx,t,h_d_apx); %hold on; yline(env.h_d_apx+env.y_imp+env.d,'.-r')
+legend('real apex','desired apex')
+title('real apex height based on post impact velocity vs desired apex height');
 figure();
 plot(t,states(2,:));
 title('ball height over time')
 xlabel('t (s)'); ylabel('y (m)');
 
-bball_1_dof_animate(t,states',env.r,env.d) % uncomment to animate
-figure()
+% bball_1_dof_animate(t,states',env.r,env.d) % uncomment to animate
