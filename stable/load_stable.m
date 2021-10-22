4955b2d621c5169d2add8a0cc49ec83816d3fac9
sgillen <sgillen@ucsb.edu>
2021-10-22 12:36:14 -0700

    bug fix for load stable

--- stable/load_stable.m
+++ stable/load_stable.m
@@ -1,6 +1,7 @@
-modelDict = pyrunfile("load_stable.py", "model_dict");
+py.sys.setdlopenflags(int32(10));
+[modelDict, normDict] = pyrunfile("load_stable.py", ["model_dict", "norm_dict"]);
 algo = 'ppo';
-seed = 1;
+seed = 7;
 
 env = bball_1_dof_Env_apex_control(false);
 obs = env.reset();
@@ -8,9 +9,19 @@ obs = env.reset();
 isDone=false;
 rewards = []; 
 while ~isDone
-    ret = modelDict{algo}{seed}.predict(obs);
+    normObs = normDict{algo}{seed}.normalize_obs(obs);
+    ret = modelDict{algo}{seed}.predict(normObs);
     act = double(ret{1});
     act = double(act);
     [obs,rew,isDone,~] = env.step(act);
     rewards = [rewards, rew];
 end
+
+
+sum(rewards)
+[states, actions, t] = env.get_arrays();
+
+plot(states')
+legend("y_s", "y_b", "dy_s", "dy_b")
+figure()
+plot(actions')
--- stable/load_stable.py
+++ stable/load_stable.py
@@ -6,19 +6,23 @@ from functools import partial
 from gym import register
 from seagul.zoo3_utils import ALGOS
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import savemat
+import pickle
 
 env_script = "make_1dof_env"
 exp_path = f"/home/sgillen/work/n_link_arm/stable/sb3_data/{env_script}"
 
+env_partial = partial(MatlabGymMmapWrapper, "/home/sgillen/work/n_link_arm/", env_script)
 
 try:
-    env_partial = partial(MatlabGymMmapWrapper, "/home/sgillen/work/n_link_arm/", env_script)
     register("bball_1dof-v0", entry_point = env_partial)
 except:
     pass
 
 model_dict = {}
-normalizer_dict = {}
+norm_dict = {}
 df_dict = {}
 
 for algo_dir in os.scandir(exp_path):
@@ -29,13 +33,22 @@ for algo_dir in os.scandir(exp_path):
         model_dict[algo_name] = []
     if not algo_name in df_dict:
         df_dict[algo_name] = []
-    if not algo_name in normalizer_dict:
-        normalizer_dict[algo_name] = []
+    if not algo_name in norm_dict:
+        norm_dict[algo_name] = []
     
     for seed_dir in os.scandir(algo_dir.path):   
         seed =  seed_dir.path.split("/")[-1]
         df_dict[algo_name].append(pd.read_csv(f"{seed_dir.path}/0.monitor.csv", skiprows=1))
         model_dict[algo_name].append(ALGO.load(f"{seed_dir.path}/model.pkl", mapped_devic='cpu'))
-        env = make_vec_env('bball_1dof-v0', n_envs = 1)
-        env = VecNormalize.load(f"{seed_dir.path}/vecnormalize.pkl", env)
-        normalizer_dict[algo_name].append(env.normalize_obs)
+    
+        norm = pickle.load(open(f"{seed_dir.path}/vecnormalize.pkl", 'rb'))
+        norm_dict[algo_name].append(norm)
+        
+        #env = make_vec_env('bball_1dof-v0', n_envs = 1)
+        #env = VecNormalize.load(f"{seed_dir.path}/vecnormalize.pkl", env)
+        #env.training = False
+        #env.norm_reward = False
+        #env_dict[algo_name].append(env)
+
+
+
